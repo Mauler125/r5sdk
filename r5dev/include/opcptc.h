a0f45c8791e4200a75e6ef0ce404c2edaa5140a2
Amos <k.mazidjatari@gmail.com>
2021-07-20 16:32:25 -0700

    Update variable names
    Removed old WPM calls after confirming the new system works

--- r5dev/include/opcptc.h
+++ r5dev/include/opcptc.h
@@ -9,10 +9,10 @@ namespace
 
 #pragma region Origin
 	/*0x14032EEA0*/
-	MemoryAddress dst000 = r5_op.PatternSearch("48 83 EC 28 80 3D ? ? ? 23 ? 0F 85 ? 02 ?");
+	MemoryAddress Origin_Init = r5_op.PatternSearch("48 83 EC 28 80 3D ? ? ? 23 ? 0F 85 ? 02 ?");
 
 	/*0x140330290*/
-	MemoryAddress dst001 = r5_op.PatternSearch("48 81 EC 58 04 ? ? 80 3D ? ? ? ? ? 0F 84");
+	MemoryAddress Origin_SetState = r5_op.PatternSearch("48 81 EC 58 04 ? ? 80 3D ? ? ? ? ? 0F 84");
 #pragma endregion
 
 #pragma region Engine
@@ -28,24 +28,24 @@ namespace
 
 #pragma region NetChannel
 	/*0x14030D000*/
-	MemoryAddress dst006 = r5_op.PatternSearch("40 55 57 41 55 41 57 48 8D AC 24 ? ? ? ?");
+	MemoryAddress CServer_Auth = r5_op.PatternSearch("40 55 57 41 55 41 57 48 8D AC 24 ? ? ? ?");
 #pragma endregion
 
 #pragma region FairFight
 	/*0x140303AE0*/
-	MemoryAddress dst007 = r5_op.PatternSearch("40 53 48 83 EC 20 8B 81 B0 03 ? ? 48 8B D9 C6");
+	MemoryAddress FairFight_Init = r5_op.PatternSearch("40 53 48 83 EC 20 8B 81 B0 03 ? ? 48 8B D9 C6");
 #pragma endregion
 
 	void PrintOAddress() // Test the sigscan results
 	{
 		std::cout << "+--------------------------------------------------------+" << std::endl;
-		PRINT_ADDRESS("dst000", dst000.GetPtr());
-		PRINT_ADDRESS("dst001", dst001.GetPtr());
+		PRINT_ADDRESS("Origin_Init", Origin_Init.GetPtr());
+		PRINT_ADDRESS("Origin_SetState", Origin_SetState.GetPtr());
 		PRINT_ADDRESS("dst002", dst002.GetPtr());
 		PRINT_ADDRESS("dst004", dst004.GetPtr());
 		PRINT_ADDRESS("Host_NewGame", Host_NewGame.GetPtr());
-		PRINT_ADDRESS("dst006", dst006.GetPtr());
-		PRINT_ADDRESS("dst007", dst007.GetPtr());
+		PRINT_ADDRESS("CServer_Auth", CServer_Auth.GetPtr());
+		PRINT_ADDRESS("FairFight_Init", FairFight_Init.GetPtr());
 		std::cout << "+--------------------------------------------------------+" << std::endl;
 
 		// TODO implement error handling when sigscan fails or result is 0
--- r5dev/src/opcptc.cpp
+++ r5dev/src/opcptc.cpp
@@ -9,10 +9,8 @@ void InstallOpcodes() /* .TEXT */
 {
 	//-------------------------------------------------------------------------
 	// JNZ --> JMP | Prevent OriginSDK from initializing on the client
-	//WriteProcessMemory(GameProcess, LPVOID(dst000 + 0x0B), "\xE9\x63\x02\x00\x00\x00", 6, NULL);
-	//WriteProcessMemory(GameProcess, LPVOID(dst001 + 0x0E), "\xE9\xCB\x03\x00\x00", 5, NULL);
-	//dst000.Offset(0x0B).Patch({ 0xE9, 0x63, 0x02, 0x00, 0x00, 0x00 });
-	//dst001.Offset(0x0E).Patch({ 0xE9, 0xCB, 0x03, 0x00, 0x00 }); 
+	//Origin_Init.Offset(0x0B).Patch({ 0xE9, 0x63, 0x02, 0x00, 0x00, 0x00 });
+	//Origin_SetState.Offset(0x0E).Patch({ 0xE9, 0xCB, 0x03, 0x00, 0x00 });
 	//-------------------------------------------------------------------------
 	// JNE --> JMP | Allow games to be loaded without the optional texture streaming file
 	dst002.Offset(0x8E5).Patch({ 0xEB, 0x19 });
@@ -21,12 +19,11 @@ void InstallOpcodes() /* .TEXT */
 	dst004.Offset(0x1D6).Patch({ 0xEB, 0x27 });
 	//-------------------------------------------------------------------------
 	// JNE --> JMP | Prevent connect localhost from being executed after listenserver init
-	//WriteProcessMemory(GameProcess, LPVOID(Host_NewGame + 0x637), "\xE9\xC1\x00\x00\x00", 5, NULL);
 	//Host_NewGame.Offset(0x637).Patch({ 0xE9, 0xC1, 0x00, 0x00, 0x00});
 	//-------------------------------------------------------------------------
 	// JA  --> JMP | Disable server-side verification for duplicate accounts on the server
-	dst006.Offset(0x284).Patch({ 0x90, 0x90 });
+	CServer_Auth.Offset(0x284).Patch({ 0x90, 0x90 });
 	//-------------------------------------------------------------------------
 	// JA  --> JMP | Prevent FairFight anti-cheat from initializing on the server
-	dst007.Offset(0x61).Patch({ 0xE9, 0xED, 0x00, 0x00, 0x00, 0x00 });
+	FairFight_Init.Offset(0x61).Patch({ 0xE9, 0xED, 0x00, 0x00, 0x00, 0x00 });
 }
