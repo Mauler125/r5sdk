6d4f7e345f38872ce45751604b40871b969cec8f
Amos <48657826+Mauler125@users.noreply.github.com>
2021-12-30 02:47:42 +0100

    Fix a typo

--- r5dev/engine/debugoverlay.cpp
+++ r5dev/engine/debugoverlay.cpp
@@ -8,8 +8,8 @@
 void DebugOverlays_Init()
 {
 #if defined (GAMEDLL_S0) || defined (GAMEDLL_S1)
-	p_DrawAllOverlays.Offset(0x189).Patch({ 0x83, 0x3F, 0x02 });  // Default value in memory is 0x2, condition is 0x4. Patch to fullfill condition.
+	p_DrawAllOverlays.Offset(0x189).Patch({ 0x83, 0x3F, 0x02 });  // Default value in memory is 0x2, condition is 0x4. Patch to fulfill condition.
 #elif defined (GAMEDLL_S2) || defined (GAMEDLL_S3)
-	p_DrawAllOverlays.Offset(0x188).Patch({ 0x83, 0x3F, 0x02 });  // Default value in memory is 0x2, condition is 0x4. Patch to fullfill condition.
+	p_DrawAllOverlays.Offset(0x188).Patch({ 0x83, 0x3F, 0x02 });  // Default value in memory is 0x2, condition is 0x4. Patch to fulfill condition.
 #endif
 }
