604e25e04698dddf552b1c4928bdb8111a20931c
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2022-05-20 02:46:18 +0200

    Fix mistake

--- r5dev/tier0/jobthread.h
+++ r5dev/tier0/jobthread.h
@@ -24,7 +24,7 @@ class VJobThread : public IDetour
 {
 	virtual void GetAdr(void) const
 	{
-		spdlog::debug("| FUN: JT_ParallelCall                      : {:#18x} |\n", p_JT_HelpWithAnything.GetPtr());
+		spdlog::debug("| FUN: JT_ParallelCall                      : {:#18x} |\n", p_JT_ParallelCall.GetPtr());
 		spdlog::debug("| FUN: JT_HelpWithAnything                  : {:#18x} |\n", p_JT_HelpWithAnything.GetPtr());
 		spdlog::debug("| FUN: JT_AcquireFifoLock                   : {:#18x} |\n", p_JT_AcquireFifoLock.GetPtr());
 		spdlog::debug("| FUN: JT_ReleaseFifoLock                   : {:#18x} |\n", p_JT_ReleaseFifoLock.GetPtr());
