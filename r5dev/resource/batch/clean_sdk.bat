6b6ee089f641a1b05345c4bf7ed49c52cba617d2
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2022-05-28 17:14:42 +0200

    Add cleaner batch script
    With the 'Clean SDK' button in the new GUI launcher, the user could remove all redundant files.

---
+++ r5dev/resource/batch/clean_sdk.bat
@@ -0,0 +1,14 @@
+REM Remove log files ('log' is no longer used. 'logs' contains current logs, these get automatically cleaned if they exceed 10mb).
+rd /S /Q "%~dp0log"
+rd /S /Q "%~dp0logs"
+REM Remove old navmesh files which where included as an attempt to debug/suppress warnings.
+rd /S /Q "%~dp0platform\maps\graphs"
+rd /S /Q "%~dp0platform\maps\navmesh"
+REM Remove deprecated binary and configuration files (these are no longer used).
+del /Q "%~dp0..\banlist.config"
+del /Q "%~dp0..\gui.config"
+del /Q "%~dp0..\Run R5 Reloaded.exe"
+del /Q "%~dp0..\r5reloaded.exe"
+del /Q "%~dp0..\r5apexsdkd64.dll"
+del /Q "%~dp0..\r5detours.dll"
+del /Q "%~dp0..\r5dev.dll"
