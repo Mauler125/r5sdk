39dd70f929811d765376792d08d8a148ec89b06b
Amos <k.mazidjatari@gmail.com>
2022-06-15 13:33:47 +0200

    Update client.cpp comments
    client.cpp (holding CClient) is in 'engine/client' based on asserts from the game module

--- r5dev/engine/client/client.cpp
+++ r5dev/engine/client/client.cpp
@@ -6,7 +6,6 @@
 //
 //===============================================================================//
 // client.cpp: implementation of the CClient class.
-// This is the server implementation, maybe we should move it to engine/server ?
 ///////////////////////////////////////////////////////////////////////////////////
 #include "core/stdafx.h"
 #include "engine/client/client.h"
