e1ede0c995730ba50ceea2800ad8eb9c682cf200
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2022-06-24 12:12:00 +0200

    Remove unused function from imgui_logger.cpp

--- r5dev/thirdparty/imgui/src/imgui_logger.cpp
+++ r5dev/thirdparty/imgui/src/imgui_logger.cpp
@@ -134,44 +134,6 @@ static int UTF8CharLength(CTextLogger::Char c)
 	return 1;
 }
 
-// "Borrowed" from ImGui source
-static inline int ImTextCharToUtf8(char* buf, int buf_size, unsigned int c)
-{
-	if (c < 0x80)
-	{
-		buf[0] = (char)c;
-		return 1;
-	}
-	if (c < 0x800)
-	{
-		if (buf_size < 2) return 0;
-		buf[0] = (char)(0xc0 + (c >> 6));
-		buf[1] = (char)(0x80 + (c & 0x3f));
-		return 2;
-	}
-	if (c >= 0xdc00 && c < 0xe000)
-	{
-		return 0;
-	}
-	if (c >= 0xd800 && c < 0xdc00)
-	{
-		if (buf_size < 4) return 0;
-		buf[0] = (char)(0xf0 + (c >> 18));
-		buf[1] = (char)(0x80 + ((c >> 12) & 0x3f));
-		buf[2] = (char)(0x80 + ((c >> 6) & 0x3f));
-		buf[3] = (char)(0x80 + ((c) & 0x3f));
-		return 4;
-	}
-	//else if (c < 0x10000)
-	{
-		if (buf_size < 3) return 0;
-		buf[0] = (char)(0xe0 + (c >> 12));
-		buf[1] = (char)(0x80 + ((c >> 6) & 0x3f));
-		buf[2] = (char)(0x80 + ((c) & 0x3f));
-		return 3;
-	}
-}
-
 void CTextLogger::Advance(Coordinates & aCoordinates) const
 {
 	if (aCoordinates.m_nLine < static_cast<int>(m_Lines.size()))
