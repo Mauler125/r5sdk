3efd7ece3bf18305104f21d798b3c1384e1d6196
PixieCore <41352111+IcePixelx@users.noreply.github.com>
2022-07-01 22:53:46 +0200

    Changed a datatype in CShaderGlue

--- r5dev/materialsystem/cshaderglue.h
+++ r5dev/materialsystem/cshaderglue.h
@@ -10,7 +10,7 @@ public:
 	uint16_t m_nCount3; //0x000C
 	uint8_t m_nByte1; //0x000E
 	uint8_t m_nByte2; //0x000F
-	char pad_0010[32]; //0x0010 [ PIXIE ]: Unknown, Due to compiler deciding to copy over 16 bytes at once in the replace function I have no clue what the data size is.
+	uint8_t pad_0010[32]; //0x0010 [ PIXIE ]: Unknown, Due to compiler deciding to copy over 16 bytes at once in the replace function I have no clue what the data size is.
 	void* m_pVertexShader; //0x0030 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
 	void* m_pPixelShader; //0x0038 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
 }; //Size: 0x0040
