1e2bfe6dd216aea65a3b3fa73e043e6c7b4a9203
PixieCore <41352111+IcePixelx@users.noreply.github.com>
2022-07-03 21:55:35 +0200

    90% Fully reversed CShaderGlue

--- r5dev/materialsystem/cshaderglue.h
+++ r5dev/materialsystem/cshaderglue.h
@@ -1,21 +1,32 @@
 #pragma once
 
 #pragma pack(push, 1)
-class CShaderGlue
+class CShaderGlue // Most of these found in CShaderGlue::SetupShader
 {
 public:
 	void* m_pVTable; //0x0000
-	uint16_t m_nCount1; //0x0008
-	uint16_t m_nCount2; //0x000A
-	uint16_t m_nCount3; //0x000C
-	uint8_t m_nByte1; //0x000E
-	uint8_t m_nByte2; //0x000F
-	uint8_t pad_0010[32]; //0x0010 [ PIXIE ]: Unknown, Due to compiler deciding to copy over 16 bytes at once in the replace function I have no clue what the data size is.
-	void** m_pVertexShader; //0x0030 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
-	void** m_pPixelShader; //0x0038 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
+	char pad_0008[8]; //0x0008
+	uint64_t m_nUnknown1; //0x0010
+	uint16_t m_nCount1; //0x0018
+	uint16_t m_nCount2; //0x001A
+	uint16_t m_nNumSamplers; //0x001C [ PIXIE ]: Used by ID3D11DeviceContext::PSSetSamplers to set NumSamplers
+	uint8_t m_nStartSlot; //0x001E [ PIXIE ]: Used by ID3D11DeviceContext::PSSetShaderResources to set StartSlot.
+	uint8_t m_nNumViews; //0x001F [ PIXIE ]: Used by ID3D11DeviceContext::PSSetShaderResources to set NumViews.
+	uint8_t m_nByte1; //0x0020 [ PIXIE ]: No clue tbh the only usage is an example I will show below. All of the usages are cmp also..?
+	uint8_t pad_0021[15]; //0x0021
+	void** m_ppVertexShader; //0x0030 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
+	void** m_ppPixelShader; //0x0038 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
 }; //Size: 0x0040
-static_assert(sizeof(CShaderGlue) == 0x40);
+static_assert(sizeof(CShaderGlue) == 0x40); // [ PIXIE ]: All vars have proper datatype size, paddings are unknown.
 #pragma pack(pop)
+
+/*
+  if ( *(_BYTE *)(v19 + a1 + 32) != byte_14171A08A ) // (v19 + a1 + 32) = m_nByte1
+  {
+	byte_14171A08A = *(_BYTE *)(v19 + a1 + 32);
+	qword_14171AE78 = -1i64;
+  }
+*/
 
 /* ==== CSHADERGLUE ================================================================================================================================================== */
 
