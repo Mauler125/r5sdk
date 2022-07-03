6a90314fc101ae9acba36a890a9983042875a428
PixieCore <41352111+IcePixelx@users.noreply.github.com>
2022-07-03 13:57:24 +0200

    Update cshaderglue.h

--- r5dev/materialsystem/cshaderglue.h
+++ r5dev/materialsystem/cshaderglue.h
@@ -11,8 +11,8 @@ public:
 	uint8_t m_nByte1; //0x000E
 	uint8_t m_nByte2; //0x000F
 	uint8_t pad_0010[32]; //0x0010 [ PIXIE ]: Unknown, Due to compiler deciding to copy over 16 bytes at once in the replace function I have no clue what the data size is.
-	void* m_pVertexShader; //0x0030 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
-	void* m_pPixelShader; //0x0038 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
+	void** m_pVertexShader; //0x0030 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
+	void** m_pPixelShader; //0x0038 [ PIXIE ]: Points to another structure which holds a double ptr to d3d11.dll
 }; //Size: 0x0040
 static_assert(sizeof(CShaderGlue) == 0x40);
 #pragma pack(pop)
