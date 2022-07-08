6b7a1f62e605f225dcdf717c1bea5faa6f1e1b31
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2022-07-08 20:18:40 +0200

    Fix declaration
    Add missing optimizations belonging to previous commit.

--- r5dev/tier2/renderutils.h
+++ r5dev/tier2/renderutils.h
@@ -2,8 +2,7 @@
 #define RENDERUTILS_H
 #include "mathlib/vector.h"
 
-void DrawAngledBox(Vector3D org, QAngle ang, Vector3D mins, Vector3D maxs, int r, int g, int b, int a, bool throughSolid);
-
+void DrawAngledBox(const Vector3D& origin, const QAngle& angles, Vector3D mins, Vector3D maxs, int r, int g, int b, int a, bool throughSolid);
 void RenderCapsule(const Vector3D& vStart, const Vector3D& vEnd, const float& flRadius, Color c);
 
 #endif // RENDERUTILS_H
