83236940fadd428710a85c431f22e00147296783
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2022-07-29 17:43:24 +0200

    Update max worldsize coords
    MAX_COORD has been increased significantly in this engine.
    Not sure yet if the rest stayed the same.

--- r5dev/public/include/worldsize.h
+++ r5dev/public/include/worldsize.h
@@ -14,14 +14,14 @@
 
 // These definitions must match the coordinate message sizes in coordsize.h
 
-// Following values should be +16384, -16384, +15/16, -15/16
+// Following values should be +65536, -65536, +15/16, -15/16
 // NOTE THAT IF THIS GOES ANY BIGGER THEN DISK NODES/LEAVES CANNOT USE SHORTS TO STORE THE BOUNDS
-#define MAX_COORD_INTEGER			(16384)
+#define MAX_COORD_INTEGER			(65536)
 #define MIN_COORD_INTEGER			(-MAX_COORD_INTEGER)
 #define MAX_COORD_FRACTION			(1.0-(1.0/16.0))
 #define MIN_COORD_FRACTION			(-1.0+(1.0/16.0))
 
-#define MAX_COORD_FLOAT				(16384.0f)
+#define MAX_COORD_FLOAT				(65536.0f)
 #define MIN_COORD_FLOAT				(-MAX_COORD_FLOAT)
 
 // Width of the coord system, which is TOO BIG to send as a client/server coordinate value
