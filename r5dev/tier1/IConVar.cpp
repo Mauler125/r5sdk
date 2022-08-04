df81a1e277add438e09a7d0197c7538b2950aa76
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2022-08-04 17:56:54 +0200

    Update rui_drawEnable help text

--- r5dev/tier1/IConVar.cpp
+++ r5dev/tier1/IConVar.cpp
@@ -187,7 +187,7 @@ void ConVar::Init(void) const
 	//-------------------------------------------------------------------------
 	// RUI                                                                    |
 #ifndef DEDICATED
-	rui_drawEnable = new ConVar("rui_drawEnable", "1", FCVAR_RELEASE, "Draws the RUI, 1 = Draw, 0 = No Draw.", false, 0.f, false, 0.f, nullptr, nullptr);
+	rui_drawEnable = new ConVar("rui_drawEnable", "1", FCVAR_RELEASE, "Draws the RUI if set.", false, 0.f, false, 0.f, nullptr, " 1 = Draw, 0 = No Draw.");
 #endif // !DEDICATED
 	//-------------------------------------------------------------------------
 }
