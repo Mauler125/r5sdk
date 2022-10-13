2059e23738ba65806da8be6d082921fd1ac72d14
Marvin D <41352111+IcePixelx@users.noreply.github.com>
2022-10-13 17:03:51 +0200

    TraceRay with filter

--- r5dev/engine/enginetrace.h
+++ r5dev/engine/enginetrace.h
@@ -65,7 +65,7 @@ class CEngineTrace
 	virtual void stub_0() const = 0;
 	virtual void stub_1() const = 0;
 	virtual void ClipRayToCollideable(__m128* a2, unsigned int a3, __int64* a4, void* a5) = 0;
-	virtual void stub_3() const = 0;
+	virtual void TraceRay(const Ray_t& ray, unsigned int fMask, void* tracefilter, trace_t pTrace) = 0;
 	virtual void TraceRay(const Ray_t& ray, unsigned int fMask, trace_t pTrace) = 0;
 };
 
