70c64202b8b2cec71599eb29fd0b0019923d4f1d
rexx <67599507+r-ex@users.noreply.github.com>
2022-11-27 16:19:29 +0000

    added Miles_Initialize print

--- r5dev/milessdk/shared/core.h
+++ r5dev/milessdk/shared/core.h
@@ -4,6 +4,9 @@
 inline CMemory p_AIL_LogFunc;
 inline auto v_AIL_LogFunc = p_AIL_LogFunc.RCast<void(*)(int64_t nLogLevel, const char* pszMessage)>();
 
+inline CMemory p_Miles_Initialize;
+inline auto v_Miles_Initialize = p_Miles_Initialize.RCast<bool(*)()>();
+
 void MilesCore_Attach();
 void MilesCore_Detach();
 
@@ -20,6 +23,10 @@ class MilesCore : public IDetour
 		p_AIL_LogFunc = g_GameDll.FindPatternSIMD(reinterpret_cast<rsig_t>("\x40\x53\x48\x83\xEC\x20\x48\x8B\xDA\x48\x8D\x15\x00\x00\x00\x00"), "xxxxxxxxxxxx????");
 		v_AIL_LogFunc = p_AIL_LogFunc.RCast<void(*)(int64_t, const char*)>();
 		// 0x1409D1420 // 40 53 48 83 EC 20 48 8B DA 48 8D 15 ? ? ? ? //
+
+		p_Miles_Initialize = g_GameDll.FindPatternSIMD(reinterpret_cast<rsig_t>("\xE8\x00\x00\x00\x00\xFF\x0D\x00\x00\x00\x00\xC6\x05\x00\x00\x00\x00\x00"), "x????xx????xx?????").FollowNearCallSelf();;
+		v_Miles_Initialize = p_Miles_Initialize.RCast<bool(*)()>();
+		// 0x14095A140 // E8 ? ? ? ? FF 0D ? ? ? ? C6 05 ? ? ? ? ?  //
 	}
 	virtual void GetVar(void) const { }
 	virtual void GetCon(void) const { }
