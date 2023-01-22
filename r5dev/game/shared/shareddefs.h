d7f3209525a13da3118c4e5cd9415353a70074c4
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-01-22 12:06:05 +0100

    Mapped out hit group standards
    Mapped out from Squirrel code.

--- r5dev/game/shared/shareddefs.h
+++ r5dev/game/shared/shareddefs.h
@@ -24,4 +24,20 @@
 #endif // !CLIENT_DLL
 
 
+// ---------------------------
+//  Hit Group standards
+// ---------------------------
+#define HITGROUP_INVALID	-1
+#define	HITGROUP_GENERIC	0
+#define	HITGROUP_HEAD		1
+#define	HITGROUP_CHEST		2
+#define	HITGROUP_STOMACH	3
+#define HITGROUP_LEFTARM	4	
+#define HITGROUP_RIGHTARM	5
+#define HITGROUP_LEFTLEG	6
+#define HITGROUP_RIGHTLEG	7
+#define HITGROUP_GEAR		8			// alerts NPC, but doesn't do damage or bleed (1/100th damage)
+#define HITGROUP_COUNT		9
+
+
 #endif // SHAREDDEFS_H
