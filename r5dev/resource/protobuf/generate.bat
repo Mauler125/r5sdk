608f14e920833a218776ca9c28364d6bcaa7c5cd
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-03-27 01:37:15 +0200

    Update protobuf source files
    Added new field 'messageType'; renamed 'responseID' to 'messageID', renamed 'responseBuf' to 'responseMsg'.

--- r5dev/resource/protobuf/generate.bat
+++ r5dev/resource/protobuf/generate.bat
@@ -1,3 +1,3 @@
-protoc --cpp_out=. sig_map.proto
-protoc --cpp_out=. sv_rcon.proto
-protoc --cpp_out=. cl_rcon.proto
+protoc64 --cpp_out=. sig_map.proto
+protoc64 --cpp_out=. sv_rcon.proto
+protoc64 --cpp_out=. cl_rcon.proto
