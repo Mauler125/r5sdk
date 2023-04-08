f62092d6f280ff3d635065b226d610d8a3a1aa8f
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-04-08 18:28:39 +0200

    Fix sign of array subscript
    Must be unsigned to avoid indexing on negative numbers.

--- r5dev/tier1/characterset.cpp
+++ r5dev/tier1/characterset.cpp
@@ -15,7 +15,7 @@
 #include "tier1/characterset.h"
 
 // memdbgon must be the last include file in a .cpp file!!!
-//#include "tier0/memdbgon.h"
+#include "tier0/memdbgon.h"
 
 //-----------------------------------------------------------------------------
 // Purpose: builds a simple lookup table of a group of important characters
@@ -24,7 +24,7 @@
 //-----------------------------------------------------------------------------
 void CharacterSetBuild(characterset_t* pSetBuffer, const char* pszSetString)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	// Test our pointers
 	if (!pSetBuffer || !pszSetString)
@@ -34,7 +34,8 @@ void CharacterSetBuild(characterset_t* pSetBuffer, const char* pszSetString)
 
 	while (pszSetString[i])
 	{
-		pSetBuffer->set[pszSetString[i]] = 1;
+		unsigned char ch = (unsigned char)pszSetString[i];
+		pSetBuffer->set[ch] = 1;
 		i++;
 	}
 }
