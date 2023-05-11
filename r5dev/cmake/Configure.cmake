ce0ce28040a673bf1f5dd0e02db1c5458b07bf05
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-05-11 23:50:06 +0200

    Cleanup main CMakeLists
    Move init to 'Configure.cmake', and enable the use of folders within project files.

--- r5dev/cmake/Configure.cmake
+++ r5dev/cmake/Configure.cmake
@@ -1,7 +1,19 @@
+# -----------------------------------------------------------------------------
+# Initial setup for build system
+# -----------------------------------------------------------------------------
+macro( initial_setup )
+    set( CMAKE_CXX_STANDARD 17 )
+    set( CMAKE_CXX_STANDARD_REQUIRED True )
+
+    set( ENGINE_SOURCE_DIR ${CMAKE_SOURCE_DIR}/r5dev )
+    set( GLOBAL_PCH ${ENGINE_SOURCE_DIR}/core/stdafx.h ) # Global precompiled header shared among all libraries
+
+    set_property( GLOBAL PROPERTY USE_FOLDERS ON ) # Use filters
+endmacro()
 
 # -----------------------------------------------------------------------------
 # Set global configuration types
 # -----------------------------------------------------------------------------
 macro( setup_build_configurations )
-set( CMAKE_CONFIGURATION_TYPES "Debug;Profile;Release" CACHE STRING "" FORCE )
+    set( CMAKE_CONFIGURATION_TYPES "Debug;Profile;Release" CACHE STRING "" FORCE )
 endmacro()
