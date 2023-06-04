8e1019a265644c1adb35bd9d74002f47cee98519
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-06-05 01:45:33 +0200

    Make simple CNetAdr methods inline
    Should be inline for their simplicity to improve performance.

--- r5dev/public/tier1/NetAdr.h
+++ r5dev/public/tier1/NetAdr.h
@@ -15,29 +15,28 @@ enum class netadrtype_t
 class CNetAdr
 {
 public:
-	CNetAdr(void);
-	CNetAdr(const char* pch);
+	CNetAdr(void)            { Clear(); }
+	CNetAdr(const char* pch) { SetFromString(pch); }
 	void	Clear(void);
 
-	void	SetIP(IN6_ADDR* inAdr);
-	void	SetPort(uint16_t port);
-	void	SetType(netadrtype_t type);
+	inline void	SetIP(IN6_ADDR* inAdr)        { adr = *inAdr; }
+	inline void	SetPort(uint16_t newport)     { port = newport; }
+	inline void	SetType(netadrtype_t newtype) { type = newtype; }
 
 	bool	SetFromSockadr(struct sockaddr_storage* s);
 	bool	SetFromString(const char* pch, bool bUseDNS = false);
 
-	netadrtype_t	GetType(void) const;
-	uint16_t		GetPort(void) const;
+	inline netadrtype_t	GetType(void) const { return type; }
+	inline uint16_t		GetPort(void) const { return port; }
 
-	bool	CompareAdr(const CNetAdr& other) const;
-	bool	ComparePort(const CNetAdr& other) const;
+	bool		CompareAdr(const CNetAdr& other) const;
+	inline bool	ComparePort(const CNetAdr& other) const { return port == other.port; }
+	inline bool	IsLoopback(void) const { return type == netadrtype_t::NA_LOOPBACK; } // true if engine loopback buffers are used.
 
-	const char*		ToString(bool onlyBase = false) const;
-	void	ToString(char* pchBuffer, size_t unBufferSize, bool onlyBase = false) const;
-	void	ToAdrinfo(addrinfo* pHint) const;
-
-	void	ToSockadr(struct sockaddr_storage* s) const;
-	bool	IsLoopback(void) const; // true if engine loopback buffers are used.
+	const char*	ToString(bool onlyBase = false) const;
+	void		ToString(char* pchBuffer, size_t unBufferSize, bool onlyBase = false) const;
+	void		ToAdrinfo(addrinfo* pHint) const;
+	void		ToSockadr(struct sockaddr_storage* s) const;
 
 private:
 	netadrtype_t type;
