89092a9bb8fe0723f9f2ee511bdb5a3b63946f4e
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-07-15 21:30:42 +0200

    Update navmesh build automation script
    Copy levels navmeshes that have identical geometry.

--- r5dev/resource/batch/nav_build.bat
+++ r5dev/resource/batch/nav_build.bat
@@ -1,13 +1,26 @@
-naveditor.exe -console levels\mp_lobby.obj 1
-naveditor.exe -console levels\mp_rr_aqueduct.obj 1
-naveditor.exe -console levels\mp_rr_aqueduct_night.obj 1
-naveditor.exe -console levels\mp_rr_arena_composite.obj 1
-naveditor.exe -console levels\mp_rr_arena_skygarden.obj 1
-naveditor.exe -console levels\mp_rr_ashs_redemption.obj 1
-naveditor.exe -console levels\mp_rr_canyonlands_64k_x_64k.obj 1
-naveditor.exe -console levels\mp_rr_canyonlands_mu1.obj 1
-naveditor.exe -console levels\mp_rr_canyonlands_mu1_night.obj 1
-naveditor.exe -console levels\mp_rr_canyonlands_staging.obj 1
-naveditor.exe -console levels\mp_rr_desertlands_64k_x_64k.obj 1
-naveditor.exe -console levels\mp_rr_desertlands_64k_x_64k_nx.obj 1
-naveditor.exe -console levels\mp_rr_desertlands_64k_x_64k_tt.obj 1
+REM Build NavMesh for all levels.
+naveditor -console levels\mp_lobby.obj 1
+naveditor -console levels\mp_rr_aqueduct.obj 1
+naveditor -console levels\mp_rr_arena_composite.obj 1
+naveditor -console levels\mp_rr_arena_skygarden.obj 1
+naveditor -console levels\mp_rr_ashs_redemption.obj 1
+naveditor -console levels\mp_rr_canyonlands_64k_x_64k.obj 1
+naveditor -console levels\mp_rr_canyonlands_mu1.obj 1
+naveditor -console levels\mp_rr_canyonlands_mu1_night.obj 1
+naveditor -console levels\mp_rr_canyonlands_staging.obj 1
+naveditor -console levels\mp_rr_desertlands_64k_x_64k.obj 1
+naveditor -console levels\mp_rr_desertlands_64k_x_64k_tt.obj 1
+naveditor -console levels\mp_rr_party_crasher.obj 1
+
+REM Copy NavMesh for identical levels.
+copy /y /v %~dp0..\maps\navmesh\mp_rr_aqueduct_small.nm       %~dp0..\maps\navmesh\mp_rr_aqueduct_night_small.nm
+copy /y /v %~dp0..\maps\navmesh\mp_rr_aqueduct_med_short.nm   %~dp0..\maps\navmesh\mp_rr_aqueduct_night_med_short.nm
+copy /y /v %~dp0..\maps\navmesh\mp_rr_aqueduct_medium.nm      %~dp0..\maps\navmesh\mp_rr_aqueduct_night_medium.nm
+copy /y /v %~dp0..\maps\navmesh\mp_rr_aqueduct_large.nm       %~dp0..\maps\navmesh\mp_rr_aqueduct_night_large.nm
+copy /y /v %~dp0..\maps\navmesh\mp_rr_aqueduct_extra_large.nm %~dp0..\maps\navmesh\mp_rr_aqueduct_night_extra_large.nm
+
+copy /y /v %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_small.nm       %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_nx_small.nm
+copy /y /v %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_med_short.nm   %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_nx_med_short.nm
+copy /y /v %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_medium.nm      %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_nx_medium.nm
+copy /y /v %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_large.nm       %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_nx_large.nm
+copy /y /v %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_extra_large.nm %~dp0..\maps\navmesh\mp_rr_desertlands_64k_x_64k_nx_extra_large.nm
