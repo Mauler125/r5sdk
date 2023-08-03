b967a11ffd79b946fd3c9f5bc3a824a817873f0c
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-08-03 16:56:27 +0200

    Make switching source directories easier
    Engine src dir can now be switched with 1 var in CMake.

--- r5dev/cmake/Configure.cmake
+++ r5dev/cmake/Configure.cmake
@@ -5,9 +5,7 @@ macro( initial_setup )
     set( CMAKE_CXX_STANDARD 17 )
     set( CMAKE_CXX_STANDARD_REQUIRED True )
 
-    set( ENGINE_SOURCE_DIR "${CMAKE_SOURCE_DIR}/r5dev" CACHE PATH "Engine source directory" )
     set( THIRDPARTY_SOURCE_DIR "${ENGINE_SOURCE_DIR}/thirdparty" CACHE PATH "Thirdparty source directory" )
-
     set( BUILD_OUTPUT_DIR "game" CACHE PATH "Build output directory" )
 
     set( GLOBAL_PCH
