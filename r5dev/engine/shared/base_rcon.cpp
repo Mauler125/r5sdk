0a25f5e0fe8e8360d382345462220e7b8555460f
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-08-04 17:45:30 +0200

    Adjust 'CNetConBase' vftable order
    Order has been adjusted to match the order of execution.

--- r5dev/engine/shared/base_rcon.cpp
+++ r5dev/engine/shared/base_rcon.cpp
@@ -9,6 +9,93 @@
 #include "shared_rcon.h"
 
 //-----------------------------------------------------------------------------
+// Purpose: connect to remote
+// Input  : *pHostName - 
+//			nPort - 
+// Output : true on success, false otherwise
+//-----------------------------------------------------------------------------
+bool CNetConBase::Connect(const char* pHostName, const int nPort)
+{
+	return CL_NetConConnect(this, pHostName, nPort);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: parses input response buffer using length-prefix framing
+// Input  : &data - 
+//			*pRecvBuf - 
+//			nRecvLen - 
+//			nMaxLen - 
+// Output: true on success, false otherwise
+//-----------------------------------------------------------------------------
+bool CNetConBase::ProcessBuffer(CConnectedNetConsoleData& data, 
+	const char* pRecvBuf, int nRecvLen, const int nMaxLen)
+{
+	bool bSuccess = true;
+
+	while (nRecvLen > 0)
+	{
+		if (data.m_nPayloadLen)
+		{
+			if (data.m_nPayloadRead < data.m_nPayloadLen)
+			{
+				data.m_RecvBuffer[data.m_nPayloadRead++] = *pRecvBuf;
+
+				pRecvBuf++;
+				nRecvLen--;
+			}
+			if (data.m_nPayloadRead == data.m_nPayloadLen)
+			{
+				if (!ProcessMessage(
+					reinterpret_cast<const char*>(data.m_RecvBuffer.data()), data.m_nPayloadLen)
+					&& bSuccess)
+				{
+					bSuccess = false;
+				}
+
+				data.m_nPayloadLen = 0;
+				data.m_nPayloadRead = 0;
+			}
+		}
+		else if (data.m_nPayloadRead+1 <= sizeof(int)) // Read size field.
+		{
+			data.m_RecvBuffer[data.m_nPayloadRead++] = *pRecvBuf;
+
+			pRecvBuf++;
+			nRecvLen--;
+		}
+		else // Build prefix.
+		{
+			data.m_nPayloadLen = int(ntohl(*reinterpret_cast<u_long*>(&data.m_RecvBuffer[0])));
+			data.m_nPayloadRead = 0;
+
+			if (!data.m_bAuthorized && nMaxLen > -1)
+			{
+				if (data.m_nPayloadLen > nMaxLen)
+				{
+					Disconnect("overflow"); // Sending large messages while not authenticated.
+					return false;
+				}
+			}
+
+			if (data.m_nPayloadLen < 0 ||
+				data.m_nPayloadLen > data.m_RecvBuffer.max_size())
+			{
+				Error(eDLL_T::ENGINE, NO_ERROR, "RCON Cmd: sync error (%d)\n", data.m_nPayloadLen);
+				Disconnect("desync"); // Out of sync (irrecoverable).
+
+				return false;
+			}
+			else
+			{
+				data.m_RecvBuffer.resize(data.m_nPayloadLen);
+			}
+		}
+	}
+
+	return bSuccess;
+}
+
+//-----------------------------------------------------------------------------
 // Purpose: encode message to buffer
 // Input  : *pMsg - 
 //			*pMsgBuf - 
@@ -32,17 +119,6 @@ bool CNetConBase::Decode(google::protobuf::MessageLite* pMsg,
 	const char* pMsgBuf, const size_t nMsgLen) const
 {
 	return pMsg->ParseFromArray(pMsgBuf, int(nMsgLen));
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: connect to remote
-// Input  : *pHostName - 
-//			nPort - 
-// Output : true on success, false otherwise
-//-----------------------------------------------------------------------------
-bool CNetConBase::Connect(const char* pHostName, const int nPort)
-{
-	return CL_NetConConnect(this, pHostName, nPort);
 }
 
 //-----------------------------------------------------------------------------
@@ -123,80 +199,4 @@ void CNetConBase::Recv(CConnectedNetConsoleData& data, const int nMaxLen)
 	}
 
 	return;
-}
-
-//-----------------------------------------------------------------------------
-// Purpose: parses input response buffer using length-prefix framing
-// Input  : &data - 
-//			*pRecvBuf - 
-//			nRecvLen - 
-//			nMaxLen - 
-// Output: true on success, false otherwise
-//-----------------------------------------------------------------------------
-bool CNetConBase::ProcessBuffer(CConnectedNetConsoleData& data, 
-	const char* pRecvBuf, int nRecvLen, const int nMaxLen)
-{
-	bool bSuccess = true;
-
-	while (nRecvLen > 0)
-	{
-		if (data.m_nPayloadLen)
-		{
-			if (data.m_nPayloadRead < data.m_nPayloadLen)
-			{
-				data.m_RecvBuffer[data.m_nPayloadRead++] = *pRecvBuf;
-
-				pRecvBuf++;
-				nRecvLen--;
-			}
-			if (data.m_nPayloadRead == data.m_nPayloadLen)
-			{
-				if (!ProcessMessage(
-					reinterpret_cast<const char*>(data.m_RecvBuffer.data()), data.m_nPayloadLen)
-					&& bSuccess)
-				{
-					bSuccess = false;
-				}
-
-				data.m_nPayloadLen = 0;
-				data.m_nPayloadRead = 0;
-			}
-		}
-		else if (data.m_nPayloadRead+1 <= sizeof(int)) // Read size field.
-		{
-			data.m_RecvBuffer[data.m_nPayloadRead++] = *pRecvBuf;
-
-			pRecvBuf++;
-			nRecvLen--;
-		}
-		else // Build prefix.
-		{
-			data.m_nPayloadLen = int(ntohl(*reinterpret_cast<u_long*>(&data.m_RecvBuffer[0])));
-			data.m_nPayloadRead = 0;
-
-			if (!data.m_bAuthorized && nMaxLen > -1)
-			{
-				if (data.m_nPayloadLen > nMaxLen)
-				{
-					Disconnect("overflow"); // Sending large messages while not authenticated.
-					return false;
-				}
-			}
-
-			if (data.m_nPayloadLen < 0 ||
-				data.m_nPayloadLen > data.m_RecvBuffer.max_size())
-			{
-				Error(eDLL_T::ENGINE, NO_ERROR, "RCON Cmd: sync error (%d)\n", data.m_nPayloadLen);
-				Disconnect("desync"); // Out of sync (irrecoverable).
-
-				return false;
-			}
-			else
-			{
-				data.m_RecvBuffer.resize(data.m_nPayloadLen);
-			}
-		}
-	}
-
-	return bSuccess;
 }
