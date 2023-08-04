309e297ae4e8ba0b6b57ffc71157f6ac40989e92
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-08-04 10:49:39 +0200

    Add 'Cmd_Dispatch' to SDK
    Signature is compatible with S1.

--- r5dev/engine/cmd.h
+++ r5dev/engine/cmd.h
@@ -18,8 +18,11 @@ inline void(*Cbuf_AddText)(ECommandTarget_t eTarget, const char* pText, cmd_sour
 inline CMemory p_Cbuf_Execute;
 inline void(*Cbuf_Execute)(void);
 
+inline CMemory p_Cmd_Dispatch;
+inline void(*v_Cmd_Dispatch)(ECommandTarget_t eTarget, const ConCommandBase* pCmdBase, const CCommand* pCommand, bool bCallBackupCallback);
+
 inline CMemory p_Cmd_ForwardToServer;
-inline bool(*v_Cmd_ForwardToServer)(const CCommand* args);
+inline bool(*v_Cmd_ForwardToServer)(const CCommand* pCommand);
 
 ///////////////////////////////////////////////////////////////////////////////
 class VCmd : public IDetour
@@ -28,16 +31,20 @@ class VCmd : public IDetour
 	{
 		LogFunAdr("Cbuf_AddText", p_Cbuf_AddText.GetPtr());
 		LogFunAdr("Cbuf_Execute", p_Cbuf_Execute.GetPtr());
+		LogFunAdr("Cmd_Dispatch", p_Cmd_Dispatch.GetPtr());
 		LogFunAdr("Cmd_ForwardToServer", p_Cmd_ForwardToServer.GetPtr());
 	}
 	virtual void GetFun(void) const
 	{
 		p_Cbuf_AddText = g_GameDll.FindPatternSIMD("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 48 63 D9 41 8B F8 48 8D 0D ?? ?? ?? ?? 48 8B F2 FF 15 ?? ?? ?? ?? 48 8D 05 ?? ?? ?? ?? 41 B9 ?? ?? ?? ??");
 		p_Cbuf_Execute = g_GameDll.FindPatternSIMD("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 FF 15 ?? ?? ?? ??");
+
+		p_Cmd_Dispatch = g_GameDll.FindPatternSIMD("E8 ?? ?? ?? ?? 8B ?? 0C 49 FF C7").FollowNearCallSelf();
 		p_Cmd_ForwardToServer = g_GameDll.FindPatternSIMD("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 81 EC ?? ?? ?? ?? 44 8B 59 04");
 
 		Cbuf_AddText = p_Cbuf_AddText.RCast<void (*)(ECommandTarget_t, const char*, cmd_source_t)>(); /*48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 48 63 D9 41 8B F8 48 8D 0D ?? ?? ?? ?? 48 8B F2 FF 15 ?? ?? ?? ?? 48 8D 05 ?? ?? ?? ?? 41 B9 ?? ?? ?? ??*/
 		Cbuf_Execute = p_Cbuf_Execute.RCast<void (*)(void)>();                                        /*48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 FF 15 ?? ?? ?? ??*/
+		v_Cmd_Dispatch = p_Cmd_Dispatch.RCast<void (*)(ECommandTarget_t, const ConCommandBase*, const CCommand*, bool)>();
 		v_Cmd_ForwardToServer = p_Cmd_ForwardToServer.RCast<bool (*)(const CCommand*)>();             /*48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 57 48 81 EC ?? ?? ?? ?? 44 8B 59 04*/
 	}
 	virtual void GetVar(void) const { }
