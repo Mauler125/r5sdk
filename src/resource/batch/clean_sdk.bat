5cd7a64c8d1e43bc2eb45b79fad3fd963da95761
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2023-10-21 11:43:31 +0200

    SDK cleaner script is now in the bin dir

--- src/resource/batch/clean_sdk.bat
+++ src/resource/batch/clean_sdk.bat
@@ -1,12 +1,12 @@
 REM Remove depot files.
-rd /S /Q "%~dp0depot"
+rd /S /Q "%~dp0..\platform\depot"
 REM Remove log files ('log' is no longer used. 'logs' contains current logs, these get automatically cleaned if they exceed 10mb).
-rd /S /Q "%~dp0log"
-rd /S /Q "%~dp0logs"
+rd /S /Q "%~dp0..\platform\log"
+rd /S /Q "%~dp0..\platform\logs"
 REM Remove old NavMesh files which where included as an attempt to debug/suppress warnings.
 rd /S /Q "%~dp0..\maps"
-rd /S /Q "%~dp0maps\graphs"
-rd /S /Q "%~dp0maps\navmesh"
+rd /S /Q "%~dp0..\platform\maps\graphs"
+rd /S /Q "%~dp0..\platform\maps\navmesh"
 REM Remove deprecated binary and configuration files (these are no longer used).
 del /Q "%~dp0..\banlist.config"
 del /Q "%~dp0..\gui.config"
