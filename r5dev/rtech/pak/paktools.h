a529a16822e69627cd896585ffbdec63612ab68f
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-01-27 01:36:29 +0100

    RTech: add helpers for parsing patch header data
    Should significantly reduce noise in code

--- r5dev/rtech/pak/paktools.h
+++ r5dev/rtech/pak/paktools.h
@@ -11,4 +11,8 @@ extern PakGuid_t Pak_StringToGuid(const char* const string);
 extern const PakLoadedInfo_t* Pak_GetPakInfo(const PakHandle_t pakId);
 extern const PakLoadedInfo_t* Pak_GetPakInfo(const char* const pakName);
 
+extern PakPatchDataHeader_t* Pak_GetPatchDataHeader(PakFileHeader_t* const pakHeader);
+extern PakPatchFileHeader_t* Pak_GetPatchFileHeader(PakFileHeader_t* const pakHeader, const int index);
+extern short Pak_GetPatchNumberForIndex(PakFileHeader_t* const pakHeader, const int index);
+
 #endif // !RTECH_PAKTOOLS_H
