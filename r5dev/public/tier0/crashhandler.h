6434b8abe980175c1b3360e393ce28bf53eee971
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-02-04 00:55:07 +0100

    Tier0: crash handler refactor & bug fix
    Fix syntax error on page protection flags check when determining whether we encountered a return address. Also limit the amount of dynamic memory allocation calls, offer a large enough static buffer where we can just write into when we crash without needing to alloc or realloc anything. Also removed some hacky code like the whitelist set, and made the module handle array static into the class as well. The CrashHandler singleton is now a static class instance.

--- r5dev/public/tier0/crashhandler.h
+++ r5dev/public/tier0/crashhandler.h
@@ -1,7 +1,7 @@
 #ifndef CRASHHANDLER_H
 #define CRASHHANDLER_H
+#include "tier1/fmtstr.h"
 
-#define CRASHHANDLER_MAX_MODULES 4096
 #define CRASHMESSAGE_MSG_EXECUTABLE "bin\\crashmsg.exe"
 
 //-----------------------------------------------------------------------------
@@ -16,20 +16,18 @@ public:
 	void Init();
 	void Shutdown();
 
+	void Reset();
+
 	//-------------------------------------------------------------------------
 	// Inlines: 
 	//-------------------------------------------------------------------------
 	void Start();
 	void End();
 
-	inline void Lock() const { m_Mutex.lock(); };
-	inline void Unlock() const { m_Mutex.unlock(); };
-
-	inline void SetState(bool bState) { m_bCallState = bState; };
-	inline bool GetState() const { return m_bCallState; };
+	inline void SetExit(const bool bExit) { m_bExit = bExit; };
+	inline bool GetExit() const { return m_bExit; };
 
 	inline bool IsValid() const { return m_hExceptionHandler != nullptr; };
-	inline bool Handling() const { return m_bExceptionHandling; };
 
 	//-------------------------------------------------------------------------
 	// Formatters: 
@@ -44,17 +42,13 @@ public:
 	//-------------------------------------------------------------------------
 	// Utility: 
 	//-------------------------------------------------------------------------
-	const CHAR* ExceptionToString() const;
-	const CHAR* ExceptionToString(DWORD nExceptionCode) const;
+	const char* ExceptionToString() const;
+	const char* ExceptionToString(const DWORD nExceptionCode) const;
 
-	void SetExceptionPointers(EXCEPTION_POINTERS* pExceptionPointers) { m_pExceptionPointers = pExceptionPointers; }
+	void SetExceptionPointers(EXCEPTION_POINTERS* const pExceptionPointers) { m_pExceptionPointers = pExceptionPointers; }
 	void SetCrashCallback(PVOID pCrashCallback) { m_pCrashCallback = pCrashCallback; }
 
-	void AddWhitelist(void* pWhitelist);
-	void RemoveWhitelist(void* pWhitelist);
-	bool HasWhitelist();
-
-	void GetCallStack();
+	void CaptureCallStack();
 	void WriteFile();
 
 	void CreateMessageProcess();
@@ -66,39 +60,59 @@ private:
 	// Internals: 
 	//-------------------------------------------------------------------------
 	void FormatExceptionAddress();
-	void FormatExceptionAddress(LPCSTR pExceptionAddress);
+	void FormatExceptionAddress(const LPCSTR pExceptionAddress);
 	void FormatExceptionCode();
 
-	void FormatALU(const CHAR* pszRegister, DWORD64 nContent);
-	void FormatFPU(const CHAR* pszRegister, M128A* pxContent);
+	void FormatALU(const char* const pszRegister, const DWORD64 nContent);
+	void FormatFPU(const char* const pszRegister, const M128A* const pxContent);
 
 	bool IsPageAccessible() const;
 
 private:
 	enum
 	{
-		NUM_FRAMES_TO_SEARCH = 7,
-		NUM_FRAMES_TO_CAPTURE = 128
+		NUM_FRAMES_TO_CAPTURE = 128,
+		MAX_MODULE_HANDLES    = 4096
 	};
 
+	// Captured callstack frames.
 	PVOID m_ppStackTrace[NUM_FRAMES_TO_CAPTURE];
-	PVOID m_pCrashCallback;
-	PVOID m_hExceptionHandler;
-	EXCEPTION_POINTERS* m_pExceptionPointers;
 	WORD m_nCapturedFrames;
 
-	string m_svBuffer;       // Buffer containing the entire crash log.
-	string m_svCrashMsgInfo;
+	// The loaded module handles that are being tracked.
+	HMODULE m_ppModuleHandles[MAX_MODULE_HANDLES];
+
+	// Custom crash callback that's called after the logs have been written.
+	PVOID m_pCrashCallback;
+
+	// Current exception handler, only kept here for tracking as we need the
+	// handle if we want to remove this handler.
+	PVOID m_hExceptionHandler;
+
+	EXCEPTION_POINTERS* m_pExceptionPointers;
+
+	// 32KiB buffer containing the entire crash log, static as we shouldn't
+	// allocate any dynamic memory when writing the log files as that is
+	// unsafe during a crash.
+	CFmtStrQuietTruncationN<32768> m_Buffer;
+
+	// Buffer containing the module name we crashed in.
+	CFmtStrQuietTruncationN<256> m_CrashingModule;
+
+	// Buffer containing cmd line arguments for the external crash message
+	// process.
+	CFmtStrQuietTruncationN<256> m_MessageCmdLine;
 	uint8_t m_nCrashMsgFlags;
 
-	bool m_bCallState;        // Set when called to prevent recursive calls.
-	bool m_bCrashMsgCreated;  // Set when crashmsg.exe is created to prevent recursive messages.
-	bool m_bExceptionHandling; // Set on filter entry, unset within the same lock if exception was not handled, never unset if handled.
+	// Set when called to prevent recursive calls.
+	bool m_bExit;
 
-	std::set<void*> m_WhiteList;
-	mutable std::mutex m_Mutex;
+	// Set when crashmsg.exe is created to prevent recursive messages.
+	bool m_bMessageCreated;
+
+	mutable RTL_SRWLOCK m_Lock;
 };
 
-extern CCrashHandler* g_CrashHandler;
+extern CCrashHandler g_CrashHandler;
 
 #endif // CRASHHANDLER_H
