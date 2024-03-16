fcbf285b80a8e865e458b09f10d8d6f474d4dc94
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-03-16 11:47:59 +0100

    Miles: add global type and prevent non-existent banks from loading
    This patch adds miles globals and additional code to prevent banks from loading that don't exist. It falls back to "english" if requested language isn't installed.

--- src/codecs/miles/miles_impl.cpp
+++ src/codecs/miles/miles_impl.cpp
@@ -1,9 +1,11 @@
 #include "core/stdafx.h"
 #include "miles_impl.h"
 #include "tier0/fasttimer.h"
+#include "tier0/commandline.h"
 #include "tier1/cvar.h"
+#include "filesystem/filesystem.h"
 
-ConVar miles_debug("miles_debug", "0", FCVAR_RELEASE, "Enables debug prints for the Miles Sound System", "1 = print; 0 (zero) = no print");
+static ConVar miles_debug("miles_debug", "0", FCVAR_RELEASE, "Enables debug prints for the Miles Sound System", "1 = print; 0 (zero) = no print");
 
 //-----------------------------------------------------------------------------
 // Purpose: logs debug output emitted from the Miles Sound System
@@ -24,8 +26,24 @@ bool Miles_Initialize()
 {
 	const char* pszLanguage = miles_language->GetString();
 	if (!pszLanguage[0])
-	{
 		pszLanguage = MILES_DEFAULT_LANGUAGE;
+
+	const bool isEnglishLanguage = _stricmp(pszLanguage, "english") == 0;
+
+	if (!isEnglishLanguage)
+	{
+		const bool useShipSound = !CommandLine()->FindParm("-devsound") || CommandLine()->FindParm("-shipsound");
+
+		const std::string baseStreamFilePath = Format("%s/general_%s.mstr", useShipSound ? "audio/ship" : "audio/dev", pszLanguage);
+
+		// if the requested language for miles does not have a MSTR file present, throw a non-fatal error and force english as a fallback
+		// if we are loading english and the file is still not found, we can let it hit the regular engine error, since that is not recoverable
+		if (!FileSystem()->FileExists(baseStreamFilePath.c_str()))
+		{
+			Error(eDLL_T::AUDIO, NO_ERROR, "%s: attempted to load language '%s' but the required stream bank (%s) was not found. falling back to english...\n", pszLanguage, baseStreamFilePath.c_str());
+
+			pszLanguage = MILES_DEFAULT_LANGUAGE;
+		}
 	}
 
 	Msg(eDLL_T::AUDIO, "%s: initializing MSS with language: '%s'\n", __FUNCTION__, pszLanguage);
@@ -53,6 +71,21 @@ void MilesBankPatch(Miles::Bank* bank, char* streamPatch, char* localizedStreamP
 	v_MilesBankPatch(bank, streamPatch, localizedStreamPatch);
 }
 
+void CSOM_AddEventToQueue(const char* eventName)
+{
+	if (miles_debug.GetBool())
+		Msg(eDLL_T::AUDIO, "%s: queuing audio event '%s'\n", __FUNCTION__, eventName);
+
+	v_CSOM_AddEventToQueue(eventName);
+
+	if (g_milesGlobals->queuedEventHash == 1)
+		Warning(eDLL_T::AUDIO, "%s: failed to add event to queue; invalid event name '%s'\n", __FUNCTION__, eventName);
+
+	if (g_milesGlobals->queuedEventHash == 2)
+		Warning(eDLL_T::AUDIO, "%s: failed to add event to queue; event '%s' not found.\n", __FUNCTION__, eventName);
+};
+
+
 ///////////////////////////////////////////////////////////////////////////////
 void MilesCore::Detour(const bool bAttach) const
 {
@@ -60,4 +93,5 @@ void MilesCore::Detour(const bool bAttach) const
 	DetourSetup(&v_Miles_Initialize, &Miles_Initialize, bAttach);
 	DetourSetup(&v_MilesQueueEventRun, &MilesQueueEventRun, bAttach);
 	DetourSetup(&v_MilesBankPatch, &MilesBankPatch, bAttach);
+	DetourSetup(&v_CSOM_AddEventToQueue, &CSOM_AddEventToQueue, bAttach);
 }
