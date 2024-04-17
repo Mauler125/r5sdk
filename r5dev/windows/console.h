5d680c89d98c72d43fb45169604f6f780c60a39c
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-04-17 20:47:02 +0200

    Windows: fix prototype mismatch
    Mismatched since commit acaf3fdfad319cea142ba58ce9214aec14715ff0.

--- r5dev/windows/console.h
+++ r5dev/windows/console.h
@@ -3,6 +3,6 @@
 void SetConsoleBackgroundColor(COLORREF color);
 void FlashConsoleBackground(int nFlashCount, int nFlashInterval, COLORREF color);
 
-bool Console_Init(const bool bAnsiColor, const bool bWarn = true);
+bool Console_Init(const bool bAnsiColor);
 bool Console_ColorInit();
 bool Console_Shutdown();
