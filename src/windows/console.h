acaf3fdfad319cea142ba58ce9214aec14715ff0
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-04-17 20:44:54 +0200

    Windows: move console color init code to separate function
    Reserve Console_Init() for actual console init, if code only needs colors (e.g. a console application that already has a console), then they should call Console_ColorInit().

--- src/windows/console.h
+++ src/windows/console.h
@@ -3,5 +3,6 @@
 void SetConsoleBackgroundColor(COLORREF color);
 void FlashConsoleBackground(int nFlashCount, int nFlashInterval, COLORREF color);
 
-bool Console_Init(const bool bAnsiColor);
+bool Console_Init(const bool bAnsiColor, const bool bWarn = true);
+bool Console_ColorInit();
 bool Console_Shutdown();
