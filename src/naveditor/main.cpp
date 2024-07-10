4d94a9c1ba3b7f957f116aa29af6d3f6170db318
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-07-11 00:29:44 +0200

    Recast: align file menu's with main menu's
    Light UX improvement.

--- src/naveditor/main.cpp
+++ src/naveditor/main.cpp
@@ -1013,7 +1013,7 @@ int not_main(int argc, char** argv)
 		// Level selection dialog.
 		if (showLevels)
 		{
-			ImGui::SetNextWindowPos(ImVec2((float)width-10-250-10-300, (float)height-10-900), ImGuiCond_Once);
+			ImGui::SetNextWindowPos(ImVec2((float)width-10-250-10-300, 10.f), ImGuiCond_Once);
 			ImGui::SetNextWindowSize(ImVec2(250.f, 450.f), ImGuiCond_Once);
 			if (ImGui::Begin("Choose Level", nullptr, baseWindowFlags))
 			{
@@ -1083,7 +1083,7 @@ int not_main(int argc, char** argv)
 		// Test cases
 		if (showTestCases)
 		{
-			ImGui::SetNextWindowPos(ImVec2((float)width-10-250-10-300, (float)height-10-900), ImGuiCond_Once);
+			ImGui::SetNextWindowPos(ImVec2((float)width-10-250-10-300, 10.f), ImGuiCond_Once);
 			ImGui::SetNextWindowSize(ImVec2(250.f, 450.f), ImGuiCond_Once);
 
 			if (ImGui::Begin("Choose Test To Run", nullptr, baseWindowFlags))
