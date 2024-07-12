61705cb85e11b479ebdcd1713f88c27494597396
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-07-12 21:34:00 +0200

    Recast: add dtMathRoundf

--- src/thirdparty/recast/Detour/Include/DetourMath.h
+++ src/thirdparty/recast/Detour/Include/DetourMath.h
@@ -21,6 +21,7 @@ inline float dtMathFabsf(float x) { return fabsf(x); }
 inline float dtMathSqrtf(float x) { return sqrtf(x); }
 inline float dtMathFloorf(float x) { return floorf(x); }
 inline float dtMathCeilf(float x) { return ceilf(x); }
+inline float dtMathRoundf(float x) { return roundf(x); }
 inline float dtMathCosf(float x) { return cosf(x); }
 inline float dtMathSinf(float x) { return sinf(x); }
 inline float dtMathAtan2f(float y, float x) { return atan2f(y, x); }
