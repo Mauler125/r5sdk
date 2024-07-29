c83283a51dc5f044e0b76ec0f20209876d18a358
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-07-30 01:24:37 +0200

    Client: constify IClientEntityList interface
    All parameters can be const.

--- src/public/icliententitylist.h
+++ src/public/icliententitylist.h
@@ -8,15 +8,15 @@ class C_BaseEntity;
 class IClientEntityList
 {
 public:
-	virtual C_BaseEntity*       GetBaseEntity(int entNum) = 0;
-	virtual IClientNetworkable* GetClientNetworkable(int entNum) = 0;
+	virtual C_BaseEntity*       GetBaseEntity(const int entNum) = 0;
+	virtual IClientNetworkable* GetClientNetworkable(const int entNum) = 0;
 	virtual IClientNetworkable* GetClientNetworkableFromHandle(const CBaseHandle& handle) = 0;
 	virtual void*               GetClientUnknownFromHandle(const CBaseHandle& handle) = 0;
-	virtual IClientEntity*      GetClientEntity(int entNum) = 0;
+	virtual IClientEntity*      GetClientEntity(const int entNum) = 0;
 	virtual IClientEntity*      GetClientEntityFromHandle(const CBaseHandle& handle) = 0;
-	virtual int                 NumberOfEntities(bool includeNonNetworkable = false) = 0;
+	virtual int                 NumberOfEntities(const bool includeNonNetworkable = false) = 0;
 	virtual int                 GetNumClientNonNetworkable() = 0;
 	virtual int                 GetHighestEntityIndex() = 0;
-	virtual void                SetMaxEntities(int maxEnts) = 0;
+	virtual void                SetMaxEntities(const int maxEnts) = 0;
 	virtual int                 GetMaxEntities() = 0;
 };
