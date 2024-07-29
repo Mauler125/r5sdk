f9e115c610d841cc5942b5930526b24f60bd95c8
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-07-29 10:22:09 +0200

    Server: update navigation types

--- src/game/server/ai_navtypes.h
+++ src/game/server/ai_navtypes.h
@@ -16,8 +16,15 @@
 // ---------------------------
 enum Navigation_e
 {
-	NAV_NONE = -1,	// error condition
-	// TODO: reverse engineer
+	NAV_NONE   = -1,// error condition
+	NAV_GROUND = 0, // walk/run
+	NAV_JUMP,       // jump/leap
+	NAV_FLY,        // can fly, move all around
+	NAV_CLIMB,      // climb ladders
+	NAV_CRAWL,      // crawl
+	NAV_TRAVERSE,   // traverse
+	NAV_WALLJUMP,   // walljump
+	NAV_WALLRUN,    // wallrun
 };
 
 #endif // AI_NAVTYPES_H
