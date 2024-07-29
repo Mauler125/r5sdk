6fc6a6706f20586f45b0d6f39163e59a24cec862
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-07-29 21:10:38 +0200

    Mathlib: fix corrupted character in comment

--- src/mathlib/mathlib.h
+++ src/mathlib/mathlib.h
@@ -2810,7 +2810,7 @@ inline bool CloseEnough(const Vector3D& a, const Vector3D& b, float epsilon = EQ
 // Fast compare
 // maxUlps is the maximum error in terms of Units in the Last Place. This 
 // specifies how big an error we are willing to accept in terms of the value
-// of the least significant digit of the floating point number�s 
+// of the least significant digit of the floating point numbers 
 // representation. maxUlps can also be interpreted in terms of how many 
 // representable floats we are willing to accept between A and B. 
 // This function will allow maxUlps-1 floats between A and B.
