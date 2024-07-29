979e9410b6bde8ccc9d7d2d0da36ca9c264f67c9
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-07-30 01:17:14 +0200

    Client: fix IClientEntityList interface
    Some methods had missing parameters, and this interface expects CBaseHandle being passed in as a reference, which explains why GetClientEntityFromHandle() didn't work properly in the past.

--- src/public/icliententitylist.h
+++ src/public/icliententitylist.h
@@ -6,12 +6,12 @@
 class IClientEntityList // Fully reversed beside index 0 which is probably a destructor.
 {
 public:
-	virtual int                 sub_1405C5E70() = 0;
-	virtual IClientNetworkable* GetClientNetworkable() = 0;
-	virtual IClientNetworkable* GetClientNetworkableFromHandle(CBaseHandle handle) = 0;
-	virtual void*               GetClientUnknownFromHandle(CBaseHandle handle) = 0;
+	virtual void*               sub_1405C5E70(int idx) = 0;
+	virtual IClientNetworkable* GetClientNetworkable(int entnum) = 0;
+	virtual IClientNetworkable* GetClientNetworkableFromHandle(const CBaseHandle& handle) = 0;
+	virtual void*               GetClientUnknownFromHandle(const CBaseHandle& handle) = 0;
 	virtual IClientEntity*      GetClientEntity(int entNum) = 0;
-	virtual IClientEntity*      GetClientEntityFromHandle(CBaseHandle handle) = 0; // behaves weird on r5 and doesn't really wanna work.
+	virtual IClientEntity*      GetClientEntityFromHandle(const CBaseHandle& handle) = 0;
 	virtual int                 NumberOfEntities(bool includeNonNetworkable = false) = 0;
 	virtual int                 GetNumClientNonNetworkable() = 0;
 	virtual int                 GetHighestEntityIndex() = 0;
