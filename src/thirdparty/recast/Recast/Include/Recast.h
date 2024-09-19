98756f1a0bc17831020b8f765cce8dbb245bcc9e
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-09-20 00:30:52 +0200

    Recast: reorder function
    Was in between area markers, move it above.

--- src/thirdparty/recast/Recast/Include/Recast.h
+++ src/thirdparty/recast/Recast/Include/Recast.h
@@ -934,6 +934,17 @@ bool rcErodeWalkableArea(rcContext* ctx, int radius, rcCompactHeightfield& chf);
 /// @returns True if the operation completed successfully.
 bool rcMedianFilterWalkableArea(rcContext* ctx, rcCompactHeightfield& chf);
 
+/// Helper function to offset voncex polygons for rcMarkConvexPolyArea.
+/// @ingroup recast
+/// @param[in]		verts		The vertices of the polygon [Form: (x, y, z) * @p nverts]
+/// @param[in]		nverts		The number of vertices in the polygon.
+/// @param[in]		offset		How much to offset the polygon by. [Units: wu]
+/// @param[out]		outVerts	The offset vertices (should hold up to 2 * @p nverts) [Form: (x, y, z) * return value]
+/// @param[in]		maxOutVerts	The max number of vertices that can be stored to @p outVerts.
+/// @returns Number of vertices in the offset polygon or 0 if too few vertices in @p outVerts.
+int rcOffsetPoly(const float* verts, const int nverts, const float offset,
+				 float* outVerts, const int maxOutVerts);
+
 /// Applies an area id to all spans within the specified bounding box. (AABB) 
 /// @ingroup recast
 /// @param[in,out]	ctx		The build context to use during the operation.
@@ -960,17 +971,6 @@ void rcMarkConvexPolyArea(rcContext* ctx, const float* verts, const int nverts,
 						  const float hmin, const float hmax, 
 						  unsigned short flags, unsigned char areaId,
 						  rcCompactHeightfield& chf);
-
-/// Helper function to offset voncex polygons for rcMarkConvexPolyArea.
-/// @ingroup recast
-/// @param[in]		verts		The vertices of the polygon [Form: (x, y, z) * @p nverts]
-/// @param[in]		nverts		The number of vertices in the polygon.
-/// @param[in]		offset		How much to offset the polygon by. [Units: wu]
-/// @param[out]		outVerts	The offset vertices (should hold up to 2 * @p nverts) [Form: (x, y, z) * return value]
-/// @param[in]		maxOutVerts	The max number of vertices that can be stored to @p outVerts.
-/// @returns Number of vertices in the offset polygon or 0 if too few vertices in @p outVerts.
-int rcOffsetPoly(const float* verts, const int nverts, const float offset,
-				 float* outVerts, const int maxOutVerts);
 
 /// Applies the area id to all spans within the specified cylinder.
 /// @ingroup recast
