455b7220f95009d74a12edf5d0daedcd6b7c793c
Kawe Mazidjatari <48657826+Mauler125@users.noreply.github.com>
2024-10-08 23:07:24 +0200

    ImGui: load and save imgui layout configuration from user cfg directory
    Keep it separate from the system configurations.

--- src/thirdparty/imgui/imgui.cpp
+++ src/thirdparty/imgui/imgui.cpp
@@ -1278,7 +1278,7 @@ ImGuiIO::ImGuiIO()
     DisplaySize = ImVec2(-1.0f, -1.0f);
     DeltaTime = 1.0f / 60.0f;
     IniSavingRate = 5.0f;
-    IniFilename = "platform\\cfg\\system\\layout.ini"; // Important: "imgui.ini" is relative to current working dir, most apps will want to lock this to an absolute path (e.g. same path as executables).
+    IniFilename = "platform\\cfg\\user\\layout.ini"; // Important: "imgui.ini" is relative to current working dir, most apps will want to lock this to an absolute path (e.g. same path as executables).
     LogFilename = "platform\\logs\\imgui_log.txt";
 #ifndef IMGUI_DISABLE_OBSOLETE_KEYIO
     for (int i = 0; i < ImGuiKey_COUNT; i++)
